72221e46d6a47d8624b987e4585e420f77427ed1
Byron Lin <byronl@Byrons-MBP.hitronhub.home>
2021-04-12 22:31:39 -0700

    Rename ledger_suffix to ledger_name

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,4 +21,4 @@ def pytest_addoption(parser):
 @pytest.fixture(scope='class', autouse=True)
 def config_variables(request):
     # Set as class attribute on the invoking test context.
-    request.cls.ledger_suffix = request.config.getoption("--ledger_suffix").replace(".", "-")
+    request.cls.ledger_name = request.config.getoption("--ledge_name").replace(".", "-")
